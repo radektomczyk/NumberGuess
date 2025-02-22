983ff541005493ff0ea2b8ed29533a76ef6980e9
Radek <radek05654@gmail.com>
2025-02-22 14:33:25 +0100

    Background image and gradient

--- App.js
+++ App.js
@@ -1,17 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 function App() {
-    return <StartGameScreen />;
+    return (
+        <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+            <StartGameScreen />;
+        </LinearGradient>
+    );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    rootScreen: {
         flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
+        // backgroundColor: "#ddb52f",
     },
 });
 
